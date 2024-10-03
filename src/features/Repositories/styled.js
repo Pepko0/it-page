20a9caa60cd5e1cb259dd9263791f2dfbc2b9730
Kacper Lewko <k.lewkosga@gmail.com>
2024-10-03 23:27:14 +0200

    added shadow for container Repositories

--- src/features/Repositories/styled.js
+++ src/features/Repositories/styled.js
@@ -3,10 +3,8 @@ import styled from "styled-components";
 export const RepositoriesContent = styled.div`
     margin: 10px;
     padding: 10px;
-    background-color: white;
-    border: solid;
-
-    
+    background-color: white; 
+    box-shadow: 5px 5px 5px 0px #EAEAEA;
 `;
 
 export const Title = styled.h2`
