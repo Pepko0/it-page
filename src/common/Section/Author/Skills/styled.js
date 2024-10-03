76054bf88cb36f0419e1a8350226f513846fcefc
Kacper Lewko <k.lewkosga@gmail.com>
2024-10-03 14:57:28 +0200

    added simple media query for Author/Skills

--- src/common/Section/Author/Skills/styled.js
+++ src/common/Section/Author/Skills/styled.js
@@ -16,6 +16,14 @@ export const Title = styled.h2`
 export const Ul = styled.ul`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
+
+    @media(max-width: ${({theme}) => theme.media.tablet}){
+      grid-template-columns: 1fr 1fr;
+    }
+
+    @media(max-width: ${({theme}) => theme.media.phone}){
+      grid-template-columns: 1fr;
+    }
 `;
 
 export const List = styled.li`
