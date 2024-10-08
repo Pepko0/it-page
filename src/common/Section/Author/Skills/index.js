33fddb3dbffbb2c94468f0655dc754a0be96598e
Kacper Lewko <k.lewkosga@gmail.com>
2024-10-08 22:50:11 +0200

    deleted bad lines in Author/Skills -> index.js

--- src/common/Section/Author/Skills/index.js
+++ src/common/Section/Author/Skills/index.js
@@ -2,33 +2,6 @@ import { Container, Title, List, Ul } from "./styled";
 
 const Skills = ({ skills }) => {
   return (
-    /*
-    <Container>
-      <Title>My skills includes:</Title>
-      <Ul>
-        <List>HTML: Semantics & accessibility</List>
-        <List>CSS BEM convention</List>
-        <List>CSS Grid & Flexbox</List>
-        <List>React Router</List>
-        <List>Working with API (fetch, axios)</List>
-        <List>NPM</List>
-
-        <List>Responsive Web Design</List>
-        <List>Promises, Async/await</List>
-        <List>React</List>
-        <List>Redux (Toolkit)</List>
-        <List>Error handling</List>
-        <List>Scrum</List>
-
-        <List>JavaScript: ES6+</List>
-        <List>Immutability</List>
-        <List>React Hooks</List>
-        <List>Redux-Saga</List>
-        <List>Git</List>
-        <List>Trello</List>
-      </Ul>
-    </Container>
-    */
     <Container>
       <Title>My skills includes</Title>
       <Ul>{skills && skills.map((skill) => <List>{skill}</List>)}</Ul>
