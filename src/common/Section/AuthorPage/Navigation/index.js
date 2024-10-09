f01596c5c4ac551560f73856c813fe7973fb146a
Kacper Lewko <k.lewkosga@gmail.com>
2024-10-09 12:58:06 +0200

    deleted empty lines in AuthorPage/Navigation

--- src/common/Section/AuthorPage/Navigation/index.js
+++ src/common/Section/AuthorPage/Navigation/index.js
@@ -5,21 +5,11 @@ const Navigation = () => {
   const location = useLocation();
 
   return (
-    
     <Container>
         {location.pathname !== "/" && <StyledLink to="/">Homepage</StyledLink>}
         {location.pathname !== "/Service" && <StyledLink to="/Service">Service</StyledLink>}
         {location.pathname !== "/Author" && <StyledLink to="/Author">Author</StyledLink>}
     </Container>
-
-
-    /*
-    <Container>
-      <StyledLink to="/">HomePage</StyledLink>
-      <StyledLink to="/Service">Współpraca</StyledLink>
-      <StyledLink to="/Author">Autor</StyledLink>
-    </Container>
-    */
   );
 };
 
