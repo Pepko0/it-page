24c552a408f7bebbddafb661fbab0b625764f832
Kacper Lewko <k.lewkosga@gmail.com>
2024-10-10 22:32:02 +0200

    added new margin for Homepage

--- src/common/Section/Homepage/Header/styled.js
+++ src/common/Section/Homepage/Header/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Header = styled.header`
   max-width: 90vw;
-  margin: 100px auto 40px;
+  margin: 20px auto 40px;
   display: grid;
   grid-template-columns: 1fr 3fr;
   grid-gap: 2vw;
