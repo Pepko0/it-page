ce7907b6b2efa7c620a52355c65838739d89d160
Kacper Lewko <k.lewkosga@gmail.com>
2024-10-16 17:47:53 +0200

    added media styles for header

--- src/common/Section/Homepage/Footer/styled.js
+++ src/common/Section/Homepage/Footer/styled.js
@@ -9,19 +9,25 @@ export const Container = styled.footer`
   padding: 10px;
   margin-top: 20px;
   bottom: 0;
+
+  @media (max-width: ${({ theme }) => theme.media.tablet}) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export const BlockRight = styled.div`
-  
   align-items: center;
   justify-content: center;
   display: flex;
 `;
 
 export const BlockLeft = styled.div`
+  display: grid;
   padding: 50px;
 
-`
+  @media (max-width: ${({ theme }) => theme.media.tablet}) {
+  }
+`;
 
 export const Title = styled.h1`
   font-family: ${({ theme }) => theme.font.dm_serif};
@@ -31,9 +37,54 @@ export const Title = styled.h1`
   margin-top: 0px;
 `;
 
-export const Text = styled.p`
-    
+export const Text = styled.p``;
+
+export const ContactLogo = styled.svg`
+  width: 40px;
+  height: 40px;
+
+  fill: ${({ theme }) => theme.color.black};
+
+  @media (max-width: ${({ theme }) => theme.media.tablet}) {
+    width: 30px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.phone}) {
+    width: 20px;
+  }
 `;
+
+export const ContactText = styled.p`
+  color: ${({ theme }) => theme.color.black};
+  font-size: 1vw;
+
+  @media (max-width: ${({ theme }) => theme.media.tablet}) {
+    font-size: 1.5vw;
+  }
+`;
+
+export const Link = styled.a`
+  text-decoration: none;
+  padding: 50px;
+  width: 18vw;
+  background-color: white;
+  border-radius: 20px 0 20px 0;
+  margin: 20px;
+  text-align: center;
+  box-shadow: 10px 10px 0px 0px #998674;
+
+  @media (max-width: ${({ theme }) => theme.media.tablet}) {
+    width: 25vw;
+    padding: 25px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.phone}) {
+    width: 30vw;
+    padding: 10px;
+  }
+`;
+
+// logo
 
 export const LogoGH = styled.svg`
   width: 75px;
@@ -75,15 +126,3 @@ export const LogoLk = styled.svg`
     width: 10vw;
   }
 `;
-
-
-export const Link = styled.a`
-    text-decoration: none; 
-    padding: 50px;
-    width: 250px;
-    background-color: white;
-    border-radius: 20px 0 20px 0;
-    margin: 20px;
-    text-align: center;
-
-`
