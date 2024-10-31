5b3730f19fec54a5b8398c1c7096dde075b1e0e8
Kacper Lewko <k.lewkosga@gmail.com>
2024-10-31 18:27:04 +0100

    added code to SocialMedia, and deleted other components with logos in other localizations

---
+++ src/common/Section/SocialMedia/styled.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const LogoGH = styled.svg`
+  width: 75px;
+  fill: ${({ theme }) => theme.color.white};
+
+  :hover {
+    fill: ${({ theme }) => theme.color.gray};
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.tablet}) {
+    width: 8vw;
+
+    :hover {
+      fill: ${({ theme }) => theme.color.black};
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.phone}) {
+    width: 10vw;
+  }
+`;
+
+export const LogoLk = styled.svg`
+  width: 75px;
+  fill: ${({ theme }) => theme.color.white};
+  :hover {
+    fill: ${({ theme }) => theme.color.linkedine};
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.tablet}) {
+    width: 8vw;
+
+    :hover {
+      fill: ${({ theme }) => theme.color.black};
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.phone}) {
+    width: 10vw;
+  }
+`;
