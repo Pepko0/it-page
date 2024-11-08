d9629a4b4627c0149088978263a3da0a7266ed49
Kacper Lewko <k.lewkosga@gmail.com>
2024-11-08 16:23:59 +0100

    added simple styles for AdminPage

--- src/common/Section/AdminPage/index.js
+++ src/common/Section/AdminPage/index.js
@@ -1,43 +1,62 @@
 import Navigation from "../Navigation";
 import { db } from "../ServicePage/firebase-config";
 import { collection, getDocs } from "firebase/firestore";
-import { Container } from "./styled";
+import { Container, ImportantText, Task, Title, Text, Items, BlockItem } from "./styled";
 import { useEffect, useState } from "react";
 
 const AdminPage = () => {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-      const fetchData = async () => {
-        const querySnapshot = await getDocs(collection(db, "mails"));
-        const dataArray = querySnapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-        }));
-        setData(dataArray);
-      };
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "mails"));
+      const dataArray = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setData(dataArray);
+    };
 
-      fetchData();
-    }, []);
-
+    fetchData();
+  }, []);
 
   return (
     <>
       <Navigation />
       <Container>
         <ul>
-            {data.map((item, index) => (
-                <li key={index}>
-                    <p>{item.from_name}</p>
-                    <p>{item.from_lastname}</p>
-                    <p>{item.from_number}</p>
-                    <p>{item.from_email}</p>
-                    <p>{item.from_company}</p>
-                    <p>{item.from_companyName}</p>
-                    <p>{item.from_dueDate}</p>
-                    <p>{item.from_message}</p>
-                </li>
-            ))}
+          {data.map((item, index) => (
+            <Task key={index}>
+              <Title>
+                {item.from_name} {item.from_lastname}
+              </Title>
+              <Items>
+                <BlockItem>
+                  <ImportantText>Phone Number: </ImportantText>
+                  <Text>{item.from_number}</Text>
+                </BlockItem>
+                <BlockItem>
+                  <ImportantText>E-mail: </ImportantText>
+                  <Text> {item.from_email}</Text>
+                </BlockItem>
+
+                <BlockItem>
+                  <ImportantText>Private or Company: </ImportantText>
+                  <Text>{item.from_company}</Text>
+                </BlockItem>
+                <BlockItem>
+                  <ImportantText>Company Name: </ImportantText>
+                  <Text>{item.from_companyName}</Text>
+                </BlockItem>
+                <BlockItem>
+                  <ImportantText>End Data: </ImportantText>
+                  <Text>{item.from_dueDate}</Text>
+                </BlockItem>
+              </Items>
+              <ImportantText>Message: </ImportantText>
+              <Text>{item.from_message}</Text>
+            </Task>
+          ))}
         </ul>
       </Container>
     </>
