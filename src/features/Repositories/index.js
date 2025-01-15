29661436bd61fda545bd893270b9473a621751ba
Kacper Lewko <k.lewkosga@gmail.com>
2025-01-15 15:11:05 +0100

    fix: change Repo with Demo in Skills

--- src/features/Repositories/index.js
+++ src/features/Repositories/index.js
@@ -1,22 +1,21 @@
 import { RepositoriesContent, Title, Content, Link, Text } from "./styled";
 
 const Repositories = ({
-    title,
-    descriptions,
-    linkPage,
-    linkRepositories,
-  }) => {
-    return (
-      <RepositoriesContent>
-        <Title>{title}</Title>
-        <Content>{descriptions}</Content>
-        <Text>Demo: </Text>
-        <Link href={linkPage}>{linkPage}</Link><br/>
-        <Text>Repo: </Text>
-        <Link href={linkRepositories}>{linkRepositories}</Link>
-      </RepositoriesContent>
-    );
-  };
-  
-  export default Repositories;
-  
+  title,
+  descriptions,
+  linkPage,
+  linkRepositories,
+}) => {
+  return (
+    <RepositoriesContent>
+      <Title>{title}</Title>
+      <Content>{descriptions}</Content>
+      <Text>Demo: </Text>
+      <Link href={linkRepositories}>{linkRepositories} <br /></Link>
+      <Text>Repo: </Text>
+      <Link href={linkPage}>{linkPage}</Link>
+    </RepositoriesContent>
+  );
+};
+
+export default Repositories;
