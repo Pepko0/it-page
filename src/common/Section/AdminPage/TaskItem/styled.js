ccd87600d18143d8d85029af0b8ec7ca32e01503
Kacper Lewko <k.lewkosga@gmail.com>
2025-01-17 00:11:46 +0100

    feat: padding on Task in AdminPage

--- src/common/Section/AdminPage/TaskItem/styled.js
+++ src/common/Section/AdminPage/TaskItem/styled.js
@@ -4,5 +4,6 @@ export const Task = styled.li`
   background-color: ${({ theme }) => theme.color.mercury};
   list-style: none;
   margin: 20px auto;
+  padding: 10px;
   box-shadow: 5px 5px 0px 0px #9b9b9b;
 `;
